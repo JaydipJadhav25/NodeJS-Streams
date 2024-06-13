749aebe569b57ea718dc16daf6642cfe532fe5a5
jaydipjadhav <jaydipdjadhav>
2024-06-13 12:32:32 +0530

    read file using create reading stream

--- index.js
+++ index.js
@@ -8,13 +8,26 @@ const app = express()
 app.use(status());
 
 
+
+
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
-fs.readFile("./sample.txt" , (err , data) =>{
-    res.end(data)
-})
+//   res.send('Hello World')
+// fs.readFile("./sample.txt" , (err , data) =>{
+//     res.end(data)
+// })
 
 
+//using streaming..
+
+//create a reading stream
+const stream = fs.createReadStream("./sample.txt" ,"utf-8");
+
+//read a data from file and send a chunk in res
+stream.on("data" , (chunk) => res.write(chunk));
+//file data end to brek a req, res cyclie
+
+stream.on("end" , () => res.end());
 
 
 })
